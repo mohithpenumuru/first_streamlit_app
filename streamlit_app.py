b55066570859b3d4f27eba3ca9c244863c1f9ab1
mohithpenumuru <52751443+mohithpenumuru@users.noreply.github.com>
2023-08-09 15:28:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit
 streamlit.title('Hi Welcome to this world')
 streamlit.header('MENU')
 streamlit.text('Roam to know more')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
