2bcedbae7c55027249b7a6ea587273b9b7bdf2a7
mohithpenumuru <52751443+mohithpenumuru@users.noreply.github.com>
2023-08-09 17:17:33 +0530

    Update streamlit_app.py
    added fruits datframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-
+import pandas
 streamlit.title('Hi Welcome to this world')
 streamlit.header('MENU')
 streamlit.text('Roam to know more')
@@ -8,3 +8,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & 🍞 Rocket Smoothie')
 streamlit.text('🥑Hard-Boiled Free-Range Egg')
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
