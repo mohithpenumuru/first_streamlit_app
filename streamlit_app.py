e2161e9e4abbf9ba3513ef79cafc3c6d675bad10
mohithpenumuru <52751443+mohithpenumuru@users.noreply.github.com>
2023-08-17 18:23:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-import pandas
-import requests
+# import pandas
+# import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('Hi Welcome to this world')
 streamlit.header('MENU')
