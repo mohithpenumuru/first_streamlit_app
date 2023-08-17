5a5b0c5c4787532b48f67b55df6a0343727cdc4c
mohithpenumuru <52751443+mohithpenumuru@users.noreply.github.com>
2023-08-17 18:31:49 +0530

    added try and catch

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,12 +26,17 @@ streamlit.dataframe(fruits_to_show)
 streamlit.dataframe(my_fruit_list)
 
 streamlit.header("Fruityvice Fruit Advice!")
-k=streamlit.text_input('What fruit would you like information about')
-streamlit.write('The User Entered is : '+k)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + k)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
-
+try:
+  fruit_choice=streamlit.text_input('What fruit would you like information about')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + k)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+    
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
